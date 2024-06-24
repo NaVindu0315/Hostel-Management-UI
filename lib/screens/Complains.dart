f9ca030c996f0f827392a102d7fb07454a83df53
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-24 19:31:53 +0530

    complains page done

--- lib/screens/Complains.dart
+++ lib/screens/Complains.dart
@@ -28,14 +28,98 @@ class _ComplainssState extends State<Complainss> {
           backgroundColor: Colors.blueAccent,
           centerTitle: true,
           title: Text(
-            'Room Availability',
+            'Complains',
             style: TextStyle(
-              fontFamily: 'RobotoMono',
-              fontSize: 20,
-              color: Colors.white,
-            ),
+                fontFamily: 'RobotoMono',
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20.0,
+          ),
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue, // Set the background color to blue
+              ),
+              padding: const EdgeInsets.all(10), // Add some padding
+              child: Text(
+                'Students Complains', // The text to display
+                style: TextStyle(
+                    color: Colors.white, // Set the font color to white
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold // Set the font size
+                    ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Center(
+              child: TextFormField(
+            maxLines: 7, // Set the maximum number of lines
+            decoration: InputDecoration(
+              filled: true, // Set the background color
+              fillColor:
+                  Colors.grey[300], // Set the background color to ash color
+              border: OutlineInputBorder(
+                // Set the border
+                borderRadius:
+                    BorderRadius.circular(10), // Set the border radius
+                borderSide: BorderSide(
+                    color: Colors.blue), // Set the border color to blue
+              ),
+              enabledBorder: OutlineInputBorder(
+                // Set the enabled border
+                borderRadius:
+                    BorderRadius.circular(10), // Set the border radius
+                borderSide: BorderSide(
+                    color: Colors.blue), // Set the border color to blue
+              ),
+              focusedBorder: OutlineInputBorder(
+                // Set the focused border
+                borderRadius:
+                    BorderRadius.circular(10), // Set the border radius
+                borderSide: BorderSide(
+                    color: Colors.blue), // Set the border color to blue
+              ),
+              labelText: 'Enter a large text',
+            ),
+          )),
+          SizedBox(
+            height: 20.0,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 270.0,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // border radius
+                  ),
+                ),
+                onPressed: () {
+                  // button press action
+                },
+                child: Text(
+                  'Send',
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+        ],
       ),
     );
   }
